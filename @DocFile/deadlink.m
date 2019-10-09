3bcce85f8f31bef55bf2af80bde5b5830abef3e2
ralfHielscher <Ralf.Hielscher@gmail.com>
2019-10-09 10:23:40 +0200

    allow deadlink to look into more the one directory

--- @DocFile/deadlink.m
+++ @DocFile/deadlink.m
@@ -1,16 +1,15 @@
 function deadlink(docFiles,outputDir)
 
 % html_path = fullfile(mtex_path,'help','html');
-html_path = outputDir;
-files = dir(fullfile(html_path,'*.html'));
-% html_page = {files.name};
+if ~iscell(outputDir), outputDir = {outputDir}; end
 
-% ug_files = get_ug_files(fullfile(mtex_path,'help'));
-check = struct;
 for file=docFiles
-  htmlFile = fullfile(outputDir,[file.sourceInfo.docName '.html']);
   
-  if exist(htmlFile)
+  htmlFile = fullfile(outputDir,[file.sourceInfo.docName '.html']);
+  path2file = cellfun(@exist,htmlFile);
+    
+  if any(path2file)
+    htmlFile = htmlFile{find(path2file,1)};
     html = read(htmlFile);
     source = read(file);
     links = regexp(html,'(href=")(?<href>\S*?)(.html")','names');
@@ -19,7 +18,7 @@ for file=docFiles
     for k=1:numel(links)
       found = strfind(source,[links(k).href '.html']);
       
-      if ~exist(fullfile(html_path,[links(k).href '.html']))
+      if ~any(cellfun(@(path) exist(fullfile(path,[links(k).href '.html'])),outputDir))
         for l=1:numel(found)                    
           disp(['<a href="matlab:opentoline(''' ...
             file.sourceFile ''',' int2str(sum(lineBreak<found(l))+1) ')">' ...
