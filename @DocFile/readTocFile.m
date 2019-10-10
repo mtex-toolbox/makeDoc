6f5b6ed4a00228733b482def1f5a694bee7a74a5
ralfHielscher <Ralf.Hielscher@gmail.com>
2019-10-10 23:23:42 +0200

    show warning for empty toc files

--- @DocFile/readTocFile.m
+++ @DocFile/readTocFile.m
@@ -1,26 +1,33 @@
 function [tocEntries,tocLabel] = readTocFile(file)
 % read the *.toc of a docFile
 %
+% Input
+%  file
+%
 % Output
-% tocEntries  - a cell--array of char with toc--Entries
-% tocLabel    - 
+%  tocEntries  - a cell--array of char with toc--Entries
+%  tocLabel    - 
+%
 
+tocEntries = {};
+tocLabel = {};
 [hasToc,tocLocation] = hasTocFile(file);
 
-if hasToc
+if ~hasToc, return; end
   
-  text = file2cell(tocLocation);
-  for k=1:numel(text)
-    line = text{k};
-    pos = regexp(line,'\w*','end');
-    if ~isempty(pos)
-      tocEntries{k} = strtrim(line(1:pos(1)));
-      tocLabel{k} = strtrim(line(pos(1)+2:end));
-    end
+text = file2cell(tocLocation);
+if isempty(text)
+  dispPerm(['No toc items in ' tocLocation])
+  return;
+end
+    
+for k=1:numel(text)
+  line = text{k};
+  pos = regexp(line,'\w*','end');
+  if ~isempty(pos)
+    tocEntries{k} = strtrim(line(1:pos(1)));
+    tocLabel{k} = strtrim(line(pos(1)+2:end));
   end
-  
-else
-  
-  tocEntries = {};
-  
+end
+
 end
