a994f129f841c0b8edf492541cef973a819ee96c
ralfHielscher <Ralf.Hielscher@gmail.com>
2019-10-10 23:22:17 +0200

    resolve the link issue

--- @DocFile/private/dom2char.m
+++ @DocFile/private/dom2char.m
@@ -5,8 +5,12 @@ if numel(s)>46
   out = s(40:end);
   out = regexprep(out,'<text>|</text>','');
   out = regexprep(out,'(\n( )*)*\n','\n');
-%   out = regexprep(['%' char(10)  out],'\n','\n% ');
-%   out = [char(10) out char(10), '%' char(10)]; % save cell end
+  % redo links
+  out = regexprep(out,'\&lt;a href="(.*?)"\&gt;(.*?)\&lt;/a\&gt;','<a href="$1">$2</a>');
+    
+  %regexprep(out,'&lt;([^\ ]+)\ ([^(&gt;)]+)&gt;','<a href="$1">$2</a>'); % make links
+  %   out = regexprep(['%' char(10)  out],'\n','\n% ');
+  %   out = [char(10) out char(10), '%' char(10)]; % save cell end
 else
   out = '';
 end
